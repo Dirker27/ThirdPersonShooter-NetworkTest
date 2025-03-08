e078fff31073d803b7606074d85e710e3bbcc15a
Dirk <hortensd@gmail.com>
2025-03-07 20:35:21 -0800

    [PC-70] Fixing syntax error in Respawn logic.

--- Source/TPS_Multiplayer/Private/Player/TPSPlayerController.cpp
+++ Source/TPS_Multiplayer/Private/Player/TPSPlayerController.cpp
@@ -15,7 +15,7 @@ void ATPSPlayerController::RequestRespawn()
 {
 	AGameModeBase* m = GetWorld()->GetAuthGameMode(); // null if not Server
 	ATPSGameMode* mode = Cast<ATPSGameMode>(m);
-	if (IsValid(mode) && mode->RequestRespawn(this));
+	if (IsValid(mode) && mode->RequestRespawn(this))
 	{
 		mode->PerformRespawn(this);
 	}
